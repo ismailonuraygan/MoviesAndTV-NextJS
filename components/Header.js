6161494d1559160fda48bb6882d6a9f2bab4533c
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-09-12 17:07:49 +0300

    Remove unnecessary comments and files

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import hulu from './hulu-white.png';
+import HeaderItem from "./HeaderItem";
+import {BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon} from '@heroicons/react/outline';
+
 function Header() {
     
     return (
-        <header className="" >
-            <h1>This is the header</h1>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center" >
+            <div className="flex flex-grow justify-evenly max-w-2xl"> 
+                <HeaderItem title="HOME" Icon = {HomeIcon} /> 
+                <HeaderItem title="TRENDING" Icon = {LightningBoltIcon} /> 
+                <HeaderItem title="VERIFIED" Icon = {BadgeCheckIcon} /> 
+                <HeaderItem title="COLLECTIONS" Icon = {CollectionIcon} /> 
+                <HeaderItem title="SEARCH" Icon = {SearchIcon} /> 
+                <HeaderItem title="ACCOUNT" Icon = {UserIcon} /> 
+            </div>
             
             <Image 
             className="object-contain"
--- utils/requests.js
+++ utils/requests.js
@@ -21,28 +21,24 @@ export default {
         title: "Horror",
         url: `discover/movie?api_key=${API_KEY}&with_genres=27`
     },
+    fetchAnimation:{
+        title: "Animation",
+        url: `discover/movie?api_key=${API_KEY}&with_genres=16`
+    },
     fetchRomanceMovies:{
         title: "Romance",
         url: `trending/all/week?api_key=${API_KEY}&with_genres=10749`
-    },
-    fetchMystery:{
-        title: "Mystery",
-        url: `discover/movie?api_key=${API_KEY}&with_genres=9648`
     },
     fetchSciFi:{
         title: "Sci-Fi",
         url: `discover/movie?api_key=${API_KEY}&with_genres=878`
     },
+    fetchMystery:{
+        title: "Mystery",
+        url: `discover/movie?api_key=${API_KEY}&with_genres=9648`
+    },
     fetchWestern:{
         title: "Western",
         url: `discover/movie?api_key=${API_KEY}&with_genres=37`
-    },
-    fetchAnimation:{
-        title: "Animation",
-        url: `discover/movie?api_key=${API_KEY}&with_genres=16`
-    },
-    fetchTV:{
-        title: "TV Movie",
-        url: `discover/movei?api_key=${API_KEY}&with_genres=10770`
     },
 }
